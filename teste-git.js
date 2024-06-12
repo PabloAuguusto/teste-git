6e777e6447d5dbed63b8546be98ab72cd734fa42
Pablo Augusto Ferraz <pabloauguusto@hotmail.com>
2024-06-12 15:10:47 -0300

    correção

--- teste-git.js
+++ teste-git.js
@@ -1,7 +1,3 @@
 console.log('Olá Mundo!'); 
 console.log('Tudo bem mundo');
-<<<<<<< HEAD
 console.log ('Mais um teste de git concluido');
-=======
-console.log ('Mais um taste de git concluido');
->>>>>>> 0febd924284bad27b6fea7924c70625e2f3ae2a8
